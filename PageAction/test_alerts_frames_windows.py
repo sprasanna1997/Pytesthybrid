0b48ac105d9bf6ddab7b32a9a9de24c627079684
chiya <sprasanna1997@gmail.com>
2024-07-18 01:05:30 +0530

    Alerts

--- PageAction/test_alerts_frames_windows.py
+++ PageAction/test_alerts_frames_windows.py
@@ -1,9 +1,19 @@
 from Pageobject.alerts_frames_windows import Alerts_Frames_Windows
+import pytest
 
 class Test_Alerts_Frames_Windows:
 
-    def test_alerts(self,setup):
+    def test_browser_window(self,setup):
         self.driver=setup
         windows_obj=Alerts_Frames_Windows(self.driver)
         windows_obj.windows()
         self.driver.quit()
+
+
+    def test_alerts(self,setup):
+        self.driver=setup
+        alerts_obj=Alerts_Frames_Windows(self.driver)
+        alerts_obj.alerts()
+        self.driver.quit()
+
+
--- Pageobject/alerts_frames_windows.py
+++ Pageobject/alerts_frames_windows.py
@@ -6,11 +6,12 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from Utility.Logging import LogGen
+from Utility.Readdata import Readdata
 
 
 #XPATHS
 
-#-------------------Alerts-------------------------------
+#-------------------Browser Windows-------------------------------
 
 alerts_btn_xpath='//button[text()=" Alerts, Frames & Windows "]'
 browserwindows_btn_xpath='//a[text()=" Browser Windows"]'
@@ -19,6 +20,13 @@ newWindow_btn_xpath='//button[text()="New Window"]'
 newWindowMsg_btn_xpath='//button[text()="New Window Message"]'
 getTxt_txt_xpath='//h1[@class="mb-3 fw-normal border-bottom text-left pb-2 mb-4"]'
 
+#-------------------Alerts-------------------------------
+alerts1_btn_xpath='//a[text()=" Alerts"]'
+alertclick_btn_xpath='//button[text()="Alert"]'
+alertdelay_btn_xpath='(//button[text()="Click Me"])[1]'
+alerttext_btn_xpath='(//button[text()="Click Me"])[2]'
+alertinput_btn_xpath='(//button[text()="Click Me"])[3]'
+alerttext_text_xpath='//div[@id="desk"]'
 
 
 class Alerts_Frames_Windows:
@@ -66,6 +74,40 @@ class Alerts_Frames_Windows:
         assert "New Window Message" == msg, "This is not a New Window Message"
         self.logger.info("******TC013-Alerts, Frames & Windows -Browser Windows- Passed*******")
 
+    def alerts(self):
+        self.wait.until(ec.element_to_be_clickable((By.XPATH, alerts_btn_xpath))).click()
+        self.wait.until(ec.element_to_be_clickable((By.XPATH, alerts1_btn_xpath))).click()
+        self.wait.until(ec.element_to_be_clickable((By.XPATH, alertclick_btn_xpath))).click()
+        alert_txt=self.driver.switch_to.alert.text
+        self.driver.switch_to.alert.accept()
+        print(alert_txt)
+        assert alert_txt=='Hello world!','Alert text is wrong'
+        self.logger.info("******TC014.1-Alerts, Frames & Windows -Alert Text Capture- Passed*******")
+
+
+        self.wait.until(ec.element_to_be_clickable((By.XPATH, alertdelay_btn_xpath))).click()
+        time.sleep(6)
+        alert_txt = self.driver.switch_to.alert.text
+        self.driver.switch_to.alert.accept()
+        print(alert_txt)
+        assert alert_txt == 'Hello just appeared', 'Alert text is wrong'
+        self.logger.info("******TC014.2-Alerts, Frames & Windows -Alerts Delay Capture- Passed*******")
+
+
+        self.wait.until(ec.element_to_be_clickable((By.XPATH, alerttext_btn_xpath))).click()
+        self.driver.switch_to.alert.accept()
+        alert_txt=self.driver.find_element(By.XPATH,alerttext_text_xpath).text
+        assert alert_txt=='You pressed OK!','Alert text is wrong'
+        self.wait.until(ec.element_to_be_clickable((By.XPATH, alerttext_btn_xpath))).click()
+        self.driver.switch_to.alert.dismiss()
+        alert_txt = self.driver.find_element(By.XPATH, alerttext_text_xpath).text
+        assert alert_txt == 'You pressed Cancel!', 'Alert text is wrong'
+        self.logger.info("******TC014.3-Alerts, Frames & Windows -Alerts Accept & Delay Capture- Passed*******")
+
+        self.wait.until(ec.element_to_be_clickable((By.XPATH, alertinput_btn_xpath))).click()
+        self.driver.switch_to.alert.send_keys(Readdata.username())
+        self.driver.switch_to.alert.accept()
+        self.logger.info("******TC014.3-Alerts, Frames & Windows -Alerts Entering Input Values- Passed*******")
 
 
 
